bbd294b2d9c498a306aacbc7dfccfc63f6801fc9
James <jamesrobertsongroves@gmail.com>
2023-01-14 12:43:49 +0000

    post done

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var passwords = require("dotenv").config();
 
 var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser');
 
+//const bodyParser = require('body-parser');
 var todoRoutes = require('./routes/todos')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
 
 app.get("/", function (req, res) {
   res.send({ Hello: "root" });
--- routes/todos.js
+++ routes/todos.js
@@ -13,7 +13,13 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-  res.send("Posted!")
+  db.Todo.create(req.body)
+  .then(function(newTodo){
+    res.status(201).json(newTodo);
+  })
+  .catch(function(err){
+    res.send(err);
+  })
 })
 
 module.exports = router;
